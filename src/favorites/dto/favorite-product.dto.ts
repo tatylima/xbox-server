1d1bff41d9509e95d78120e10ee6bb99b7499993
Tatiana Gandra <tatianagandra78@gmail.com>
2022-07-12 17:51:44 -0300

    feature: created the unfavorite products resource

---
+++ src/favorites/dto/favorite-product.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class FavoriteProductDto {
+  @IsUUID()
+  @ApiProperty({
+    description: 'Id do usuário que está favoritando o produto',
+    example: 'f11b0958-efa2-4b4f-9b84-36633e5529b8',
+  })
+  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Nome do produto que está sendo favoritado',
+    example: 'harry potter',
+  })
+  productName: string;
+}
