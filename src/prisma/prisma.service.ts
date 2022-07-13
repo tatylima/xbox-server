5998e47440352ce0172a9f62631677dc174062d5
Tatiana Gandra <tatianagandra78@gmail.com>
2022-07-12 22:36:35 -0300

    config: setup the prisma ORM

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,18 @@
 /* eslint-disable prettier/prettier */
-import {  Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService
-extends PrismaClient
-implements OnModuleInit
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient implements OnModuleInit {
+	// Garante que o banco está conectado
+	async onModuleInit() {
+		await this.$connect();
+	}
+
+	// Garante que antes de desligar a aplicação o banco será desconectado
+	async enableShutdownHooks(app: INestApplication) {
+		this.$on("beforeExit", async () => {
+			await app.close();
+		});
+	}
 }
